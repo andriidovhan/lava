30e496ace47590ebcc62c347772a0a424937cff2
Andrii Dovhan <andrii.dovhan@globallogic.com>
2018-04-10 15:13:25 +0300

    add RES variable to test.sh script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+echo -e "------------------------------------"
 echo "Something Something Something Something"
+
+if [ $? -eq 0 ]; then
+	RES="pass"
+else
+	RES="fail"
+fi
+
+lava-test-case blabla-something --result $RES
